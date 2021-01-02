2ee72dd1c5bc7fc757278f803cb015ff5fd2d330
Devesh Sharma <deveshsharma1009@gmail.com>
2021-01-02 18:53:36 +0530

    Create main.py
    This is used to find sum of N-NUMBER in ARTHIMATIC AGRESSION taken from user input.

---
+++ SUM OF N-NUMBER IN GIVEN ARTHIMATIC AGRESSION/main.py
@@ -0,0 +1,7 @@
+n= int(input("Enter The Range Of Number = "))
+a = int(input("Enter The Number From To Start = "))
+d = int(input("Enter The Pattern Of Number = "))
+z = n/2
+y = 2*a
+x = (n-1)*d
+print(z*(y+x))
