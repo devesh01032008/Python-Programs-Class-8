d165ba8baa67f60fda58ead8da4c562ad2aafd66
Devesh Sharma <deveshsharma1009@gmail.com>
2021-01-03 20:32:13 +0530

    Create main.py

---
+++ PLOT DATA ON GRAPH/SIN 0° TO 90°/main.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+from math import *
+x = [i for i in range(91)]
+x2 = [i for i in range(91)]
+y = [sin(i) for i in range(91)]
+y2 = [0 for i in range(91)]
+plt.xlabel("X-Axis")
+plt.ylabel("Y-Axis")
+plt.plot(x2,y2)
+plt.plot(x,y)
+plt.show()
