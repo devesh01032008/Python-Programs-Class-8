100720c2fa1f118c0368089afe1096934a98ebcd
Devesh Sharma <deveshsharma1009@gmail.com>
2021-01-15 20:16:40 +0530

    Create main.py
    This program will ask you username and password then you can store new username and password to it.

---
+++ LOGIN CLI APP/main.py
@@ -0,0 +1,25 @@
+question = "Who are you?"
+answer = "root"
+length = len(question)
+print(question)
+while True:
+    user_answer = input("Enter Your User Name : ")
+    if user_answer.lower() == answer:
+        password = "root"
+        user_password = input("Enter Your Password : ")
+        if user_password.lower() == password:
+        	print("Welcome "+answer)
+        	choice = input("Do you want to Add Account : ")
+        	if choice == "yes":
+        		username = input("Enter New  Username :")
+        		password = input("Enter New Password : ")
+        		print("Thank You For Adding Account ")
+        		f = open("user.txt","a")
+        		f.write(f"Username : {username}\nPassword : {password}\n")
+        		break
+        	else:
+        		break
+    elif user_answer == "quit":
+    	break
+    else:
+    	print("Wrong Username . Please Enter A Valid Username")
