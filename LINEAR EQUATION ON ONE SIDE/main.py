cce7d85b7481f79e61857c78aacda88eea02ee0c
Devesh <deveshsharma1009@gmail.com>
2021-01-21 08:23:26 +0530

    Added file

---
+++ LINEAR EQUATION ON ONE SIDE/main.py
@@ -0,0 +1,26 @@
+equation = input("Enter Equation Here = ")
+
+if "+" in equation:
+        sum = True
+else:
+        sum = False
+
+def RHSandLHS(equation):
+        LHS = equation.split("=")
+        return LHS[0],LHS[1]
+
+
+lhs,rhs = RHSandLHS(equation)
+operators = ["+","-"]
+
+
+if sum == True:
+        terms = lhs.split("+")
+        rhs = float(rhs)-float(terms[1])
+else:
+        terms = lhs.split("-")
+        rhs = float(rhs)+float(terms[1])
+answer = terms[0].split("x")
+ans = rhs/float(answer[0])
+print(f"Solution Of Given Equation = {ans}")
+print("Thank You For Using It")
