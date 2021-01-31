bb83f2b0b86aaa15b52c523643095344afecf5d5
Redmi note 8 <deveshsharma1009@gmail.com>
2021-01-31 09:04:17 +0530

    Added Clock using os module and watch command in terminal . Modified html and python file reader for work in current directory

---
+++ CLOCK AND BEEPER IN LINUX/main.py
@@ -0,0 +1,2 @@
+import os
+os.system("watch -b 1.0s 4.0s")
--- HTML AND PYTHON FILE FINDER/main.py
+++ HTML AND PYTHON FILE FINDER/main.py
@@ -1,13 +1,15 @@
 import os
-path = "//storage//emulated//0//"
-
+path = os.getcwd()
 python = []
 html = []
 # r=root, d=directories, f = files
 for r,d,f in os.walk(path):
     for file in f:
         if file.endswith(".py"):
-            python.append(file)
+            python.append(str(r)+str(d)+str(file))
         if  file.endswith(".html"):
-        	html.append(file)
-print(python,html)
+        	html.append(str(r)+str(d)+str(file))
+for file in python:
+	print(file,"\n")
+for file in html:
+	print(html,"\n")
